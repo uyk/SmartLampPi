b5cc0445083fb6927e42d5b4867ce49359a2ba94
root <meopulior@gmail.com>
2017-01-09 00:49:02 +0900

    서버로 1초마다 요청을 보냄. body를 찾지못하는 문제가 있음

--- speech.js
+++ speech.js
@@ -1,1 +1,31 @@
-var http = require("http");
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require('request');
+
+var app = express();
+var port = 5000;
+
+var optionsSmartLamp = {
+	uri : "http://218.150.183.150:3000/smartLamp",
+	method : "GET",
+	headers : { "Content-Type" : "application/x-www-form-urlencoded"},
+}
+app.use(bodyParser.urlencoded( {extended : false } ));
+
+app.get('/', function(req,res) {
+	console.log('smart void lamp');
+	res.send('Smart Voice Lamp');
+});
+
+app.listen(port,function() {
+	console.log("create Server");
+	//1초마다 smartLamp 함수 호출
+	setInterval(smartLamp,1000);
+});
+
+function smartLamp() {
+	//GET 요청 전송
+	request.get(optionsSmartLamp).on('response', function(response) {
+		console.log(response.body);
+	});
+}
