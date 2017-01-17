a5515c081dbaedb58329d22878793c4004505bdd
root <meopulior@gmail.com>
2017-01-18 03:09:47 +0900

    서버에 1초마다 요청을 보냄. 새로운 데이터가 있을 시 서버 페이지를 열어 음성 출력. 페이지를 닫는부분이 안됨.

--- speech.js
+++ speech.js
@@ -1,16 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var request = require('request');
+var fs = require('fs');
+var exec =require('child_process').exec;
 
 var app = express();
 var port = 5000;
+var defaultColor = [0,0,0];
+var newColor = [0,0,0];
+var work;
+var optionsChrome = {
+	timeout : 10000,
+	killSignal : 'SIGKILL'
+};
+
+app.use(bodyParser.urlencoded( {extended : false } ));
+app.use(bodyParser.json());
 
 var optionsSmartLamp = {
 	uri : "http://218.150.183.150:3000/smartLamp",
 	method : "GET",
 	headers : { "Content-Type" : "application/x-www-form-urlencoded"},
 }
-app.use(bodyParser.urlencoded( {extended : false } ));
 
 app.get('/', function(req,res) {
 	console.log('smart void lamp');
@@ -25,7 +36,27 @@ app.listen(port,function() {
 
 function smartLamp() {
 	//GET 요청 전송
-	request.get(optionsSmartLamp).on('response', function(response) {
-		console.log(response.body);
+	request(optionsSmartLamp, function(error, response, body) {
+		if(body == "empty" ) {
+			console.log("no data");
+			console.log("R : " + defaultColor[0]);
+			console.log("G : " + defaultColor[1]);
+			console.log("B : " + defaultColor[2]);
+		}
+		else {
+			newColor = body.split(',');
+			console.log("R : " + newColor[0]);
+			console.log("G : " + newColor[1]);
+			console.log("B : " + newColor[2]);
+
+			work = exec('chromium-browser http://218.150.183.150:3000/tts1.mp3', optionsChrome,function(err, stdout, stderr) {
+				if(err) {
+					console.log("ERROR");
+				}
+				console.log(stdout);
+			});
+			console.log(work.pid);
+			work.kill('SIGKILL');
+		}
 	});
 }
